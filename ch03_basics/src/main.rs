3eab2f9a510868efb5f7853fadd66d256b48bb3a
JinShuo-Li <ljs_2025@sjtu.edu.cn>
2026-08-28 21:57:50 +0800

    mat(ch03): add section 1's material to the repo

--- ch03_basics/src/main.rs
+++ ch03_basics/src/main.rs
@@ -1,3 +1,31 @@
+// Variables and Mutability
+fn var() {
+    let mut x = 5;
+    println!("The value of x is {x}");
+
+    // mut the value of x
+    x = 6;
+    println!("The value of x is {x}")
+}
+
+// Shadowing
+fn shadowing() {
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is {x}");
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    var();
+
+    shadowing();
+    
 }
